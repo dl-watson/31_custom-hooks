fd123c8d96ba8bc15948398f54e6268e98c23b90
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 14:25:04 -0800

    added conditional rendering to Details, filled out ItemDetails

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useCharacterByName from "../../services/useCharacterByName";
+import ItemDetails from "./ItemDetails";
 
 const Details = () => {
   const { name } = useParams();
   const { loading, character } = useCharacterByName(name);
 
-  console.log(character);
-
-  // loading ? loading... : ItemDetails
-  return <></>;
+  return loading ? <>Loading...</> : <ItemDetails character={character} />;
 };
 
 export default Details;
--- src/components/details/ItemDetails.jsx
+++ src/components/details/ItemDetails.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import titleCase from "../../utils/titleCase";
 
-const ItemDetails = () => {};
+const ItemDetails = ({ character }) => {
+  [character] = character;
+  return (
+    <div>
+      {titleCase(character.pokemon)}
+      <img src={character.url_image} alt={titleCase(character.pokemon)} />
+      <div>
+        <p>Height: {character.height}</p>
+        <p>Weight: {character.weight}</p>
+        <p>Primary Type: {character.type_1}</p>
+        <p>Secondary Type: {character.type_2}</p>
+        <p>Pokebase: {character.pokebase}</p>
+      </div>
+    </div>
+  );
+};
 
-ItemDetails.propTypes = {};
+ItemDetails.propTypes = {
+  character: PropTypes.array,
+};
 
 export default ItemDetails;
