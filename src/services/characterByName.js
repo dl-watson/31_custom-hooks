2f755f4cfae317e45a9fdcc8bebda6442d85657f
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 14:09:38 -0800

    outline next steps, shifting focus to build a fetch character by name utility

---
+++ src/services/characterByName.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+const characterByName = () => {
+  const [loading, setLoading] = useState(true);
+  const [character, setCharacter] = useState([]);
+
+  // useEffect will return fetchCharacterByName
+
+  // return loading, character
+};
+
+export default characterByName;
