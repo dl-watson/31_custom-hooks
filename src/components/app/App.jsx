3b9a91b2566fa169a3d0fa58e9cbb85098c95d58
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 13:31:04 -0800

    outline structure of Router in App.jsx, tie / path to the Main.jsx container

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "../../containers/Main";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/details/:name" component="" />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
