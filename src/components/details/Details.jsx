e7d3984a1a24f3380aa0557a970318bf832c8d96
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 14:07:33 -0800

    outline steps for Details.jsx, moving on to build a custom Hook

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const Details = () => {};
-
-Details.propTypes = {};
+const Details = () => {
+  // will use another custom hook to:
+  // intialize state: character, loading
+  // useEffect, returning a new fetch by character name
+  // loading ? loading... : ItemDetails
+};
 
 export default Details;
