ce0891a809686187e7bbe43cae596654d80a58c9
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 14:46:23 -0800

    modified ItemDetails.css and added classNames and semantic HTML to ItemDetails.jsx

--- src/components/details/ItemDetails.jsx
+++ src/components/details/ItemDetails.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import titleCase from "../../utils/titleCase";
+import styles from "./styles/ItemDetails.css";
 
 const ItemDetails = ({ character }) => {
   [character] = character;
   return (
-    <div>
-      {titleCase(character.pokemon)}
+    <main className={styles.ItemDetails}>
+      <heading>{titleCase(character.pokemon)}</heading>
       <img src={character.url_image} alt={titleCase(character.pokemon)} />
-      <div>
+      <section className={styles.details}>
         <p>Height: {character.height}</p>
         <p>Weight: {character.weight}</p>
         <p>Primary Type: {character.type_1}</p>
         <p>Secondary Type: {character.type_2}</p>
         <p>Pokebase: {character.pokebase}</p>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
