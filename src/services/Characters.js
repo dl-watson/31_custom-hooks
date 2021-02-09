94431910d889c8fde978b5cab7e076f4f88aa6f0
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 13:36:51 -0800

    outline useCharacters hook: it will initialize loading and characters state, then fetchCharacters on render

---
+++ src/services/Characters.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+const useCharacters = () => {
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+
+  // useEffect will call the fetchCharacters utility function
+
+  return { loading, characters };
+};
