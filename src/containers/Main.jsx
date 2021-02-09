fc8cfda6393885bd3d9ee3bb8140aeb61c7d19ec
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 13:34:21 -0800

    pseudocode next steps: extract container logic into a custom hook

---
+++ src/containers/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Main = () => {
+  // extract state here into a custom Hook
+  // state: characters, loading
+  // if loading === true, return a loading notification
+  // if loading === false, return a List element
+};
+
+export default Main;
