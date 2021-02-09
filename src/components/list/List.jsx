361d76d53662ceeec15c5bd049b5a69c7b750f86
dl-watson <dlarkinwatson@gmail.com>
2021-02-09 14:02:33 -0800

    titleCase pokemon name, set up basic styling

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import titleCase from "../../utils/titleCase";
 import uuid from "react-uuid";
+import styles from "./styles/List.css";
 
 const List = ({ characters }) => {
   const characterList = characters.map((character) => {
     return (
       <li key={uuid()}>
-        {character.pokemon}
+        {titleCase(character.pokemon)}
         <img src={character.url_image} />
       </li>
     );
   });
 
-  return <ul>{characterList}</ul>;
+  return <ul className={styles.List}>{characterList}</ul>;
 };
 
 List.propTypes = {
---
+++ src/utils/titleCase.js
@@ -0,0 +1,6 @@
+const titleCase = (name) =>
+  name.replace(/(^|-)(\w)/g, (input) => {
+    return input.toUpperCase();
+  });
+
+export default titleCase;
