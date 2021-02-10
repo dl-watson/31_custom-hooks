297f5ee20a99a34adda9fb121df4dcf67b6162d6
dl-watson <dlarkinwatson@gmail.com>
2021-02-10 14:25:32 -0800

    modify eslintrc, writing out pseudocode

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -7,6 +7,11 @@ import styles from "./styles/Main.css";
 const Main = () => {
   const { loading, characters, activePage, handleClick } = useCharacters();
 
+  // useContext refactor: Main.jsx should display a button that can toggle between light and dark modes
+  // to accomplish this, we'll create a context elsewhere and import it into this file
+  // the only thing that we'll end up using in this terminating component is a handler, like setTheme
+  // the theme will be exported to the Details component, which is not a child of Main.jsx
+
   return loading ? (
     <>Loading...</>
   ) : (
