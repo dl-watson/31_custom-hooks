559e9999303374e3a2e2f972b798280524155025
dl-watson <dlarkinwatson@gmail.com>
2021-02-10 16:40:55 -0800

    maybe i should check my tests before i write a commit message that suggests they are all passing

--- src/components/details/tests/ItemDetails.test.jsx
+++ src/components/details/tests/ItemDetails.test.jsx
@@ -2,12 +2,15 @@ global.fetch = require("node-fetch");
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import ItemDetails from "../ItemDetails";
+import { ThemeProvider } from "../../../providers/ThemeContext";
 
 describe("ItemDetails component", () => {
   afterEach(() => cleanup());
   it("renders ItemDetails", () => {
     const { asFragment } = render(
-      <ItemDetails character={[{ pokemon: "pokemon" }]} />
+      <ThemeProvider>
+        <ItemDetails character={[{ pokemon: "pokemon" }]} />
+      </ThemeProvider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
