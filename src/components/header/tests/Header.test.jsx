b5326f83f5ce19ecc2aaaeabd70541f78e91dae8
dl-watson <dlarkinwatson@gmail.com>
2021-02-17 08:34:05 -0800

    updated tests

--- src/components/header/tests/Header.test.jsx
+++ src/components/header/tests/Header.test.jsx
@@ -3,14 +3,17 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import Header from "../Header";
 import { ThemeProvider } from "../../../providers/ThemeContext";
+import { MemoryRouter as Router } from "react-router-dom";
 
 describe("Header component", () => {
   afterEach(() => cleanup());
   it("renders Header", () => {
     const { asFragment } = render(
-      <ThemeProvider>
-        <Header />
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider>
+          <Header />
+        </ThemeProvider>
+      </Router>
     );
     expect(asFragment()).toMatchSnapshot();
   });
